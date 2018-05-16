26132a3e1a2db5c5fb2ba10bec5f1f18fca723db
chrisrachlinski <chrisrachlinski@gmail.com>
2018-05-16 13:50:39 -0400

    Started working on test js

--- js/impl.js
+++ js/impl.js
@@ -1,3 +1,17 @@
+var wordDisp;
+var hasBegun = false;
+var sectionNum = 0;
+var startend = [4];
+var state = 0;
+
 $(document).ready(function() {
+	wordDisp = $(".display#word");
 	
+	wordDisp.text = "You are about to being the test.  Press space to continue.";
+	
+});
+
+$(document).keypress(function(e) {
+
+	startend[state] = e.timeStamp;
 });
